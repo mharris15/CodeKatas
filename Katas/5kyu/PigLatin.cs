2b10f6558ff74f889d10d03ab5ec925833e10d63
Michael Harris <michael.harris.2015@gmail.com>
2023-06-26 13:44:07 -0400

    feat: Adding PigLatin

---
+++ Katas/5kyu/PigLatin.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FileName: PigLatin.cs
+//FileType: Visual C# Source file
+//Author : Michael Harris
+//Description : Class for CodeWars problem: Simple Pig Latin
+//Link: https://www.codewars.com/kata/520b9d2ad5c005041100000f/csharp
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas._5kyu
+{
+    /// <summary>
+    /// Move the first letter of each word to the end of it, 
+    /// then add "ay" to the end of the word. Leave punctuation marks untouched.
+    /// </summary>
+    public static class PigLatin
+    {
+        public static string PigIt(string str)
+        {
+            string pigLatin = "";
+            string[] words = str.Split(' ');
+            foreach (string word in words) {
+                if (Char.IsLetter(word.ElementAt(0))){
+                    pigLatin += word.Substring(1) + word.ElementAt(0) + "ay ";
+                }
+                else
+                {
+                    pigLatin += word;
+                }
+            }
+            return pigLatin.TrimEnd();
+        }
+    }
+}
--- Katas/Program.cs
+++ Katas/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Katas._5kyu;
 
 
-List<int> test = new List<int> { 1, 2, 3, 4, 5 };
-int? n = SumOfK.chooseBestSum(11, 3, test);
 
-Console.WriteLine(n);
+Console.WriteLine(PigLatin.PigIt("Pig latin is cool"));
